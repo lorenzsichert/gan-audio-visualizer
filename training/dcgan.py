04d499fb75af4fc68d8f43540af9b9bebe716602
Lorenz Sichert <lorenz.sichert@proton.me>
2025-11-28 16:14:13 +0100

    phases

--- training/dcgan.py
+++ training/dcgan.py
@@ -1,269 +1,274 @@
-import os
-import numpy as np
-
-import torchvision.transforms as transforms
+from numpy import log2
+from torch import mean, optim, torch
+import torch.nn as nn
+from torch.utils.data import Dataset, dataset
+from torch.utils.data import DataLoader
+from datasets import load_dataset
+from torchvision import transforms
 from torchvision.utils import save_image
 
-from torchvision import datasets
 
-import torch.nn as nn
-import torch
-from datasets import load_dataset
-from PIL import Image
-
-
-
-
-opt = {
-    "n_epochs": 20000,
-    "batch_size": 1,
-    "lr": 0.0002,
-    "b1": 0.5,
-    "b2": 0.999,
-    "n_cpu": 8,
-    "latent_dim": 100,
-    "img_size": 32,
-    "channels": 3,
-    "sample_interval": 100,
-    "dataset_size": 100,
-}
+n_epochs = 2000
+b1 = 0.5
+b2 = 0.99
+latent_dim = 100
+features = 64
+init_size = 4
+img_size = 128
+layer = 1
+channels = 3
+batch_size = 16
+dataset_size = -1
+sample_interval = 64
 
 
-cuda = True if torch.cuda.is_available() else False
-device = torch.device('cuda') if cuda else torch.device('cpu')
+alpha_end = 1.5
+alpha_incease = 0.0002
+alpha_dropdown = 0.25
 
 
-def weights_init_normal(m):
-    classname = m.__class__.__name__
-    if classname.find("Conv") != -1:
-        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find("BatchNorm2d") != -1:
-        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
-        torch.nn.init.constant_(m.bias.data, 0.0)
+# --- Dataset Loading ---
+link = "mrm8488/ImageNet1K-val"
+split = "train"
+image_tag = "image"
 
+
+class DatasetTransform(Dataset):
+    def __init__(self, dataset, transform):
+        self.dataset = dataset
+        self.transform = transform
+
+    def __len__(self):
+        return self.dataset.num_rows
+
+    def __getitem__(self, index):
+        item = self.dataset[index][image_tag]
+        if self.transform:
+            item = self.transform(item)
+        return item
+
+transform = transforms.Compose([
+    transforms.Resize((img_size, img_size)),
+    transforms.Lambda(lambda x: x.convert("RGB")),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5],[0.5])
+])
+
+ds = load_dataset(link)
+train = ds[split]
+dataset = DatasetTransform(train, transform)
+dataloader = DataLoader(dataset, batch_size=batch_size)
+
+
+# --- Cuda Init ---
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+print(f"Running on {device}")
 
 class Generator(nn.Module):
-    def __init__(self, img_size, latent_dim, feature_g=64, channels=3):
+    def __init__(self, init_size, latent_dim, img_size, features, channels, layer):
         super(Generator, self).__init__()
+        self.init_size = init_size
+        self.features = features
+        self.channels = channels
+        self.layer = layer
 
-        self.init_size = 8
-        self.num_upsamples = int( np.log2(img_size // self.init_size ))
-
-        in_ch = feature_g * 2 ** self.num_upsamples
-
-        layers = []
-
-        self.fc = [
-            nn.ConvTranspose2d(latent_dim, in_ch, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(in_ch),
-            nn.ReLU(True)
-        ]
-
-        def convolutional_block(in_sample, out_sample, stride, padding):
-            return [
-                nn.ConvTranspose2d(in_sample, out_sample, kernel_size=4, stride=stride, padding=padding),
-                nn.BatchNorm2d(out_sample),
-                nn.ReLU(True),
-            ]
-
-        layers += convolutional_block(latent_dim, in_ch, stride=1, padding=0)
-
-        for _ in range(self.num_upsamples):
-            out_ch = in_ch // 2 if in_ch > feature_g else feature_g
-            layers += convolutional_block(in_ch, out_ch, stride=2, padding=1)
-            in_ch = out_ch
+        self.upscales = log2(img_size/self.init_size)
+        
+        print(self.upscales)
 
 
-        layers += [
-            nn.ConvTranspose2d(in_ch, channels, kernel_size=4, stride=2, padding=1),
-            nn.Tanh()
-        ]
+        self.network = nn.Sequential(
+            nn.ConvTranspose2d(latent_dim, self.init_size * features, kernel_size=4, stride=1, padding=0),
+            nn.InstanceNorm2d(self.init_size * features // 1),
+            nn.ReLU(inplace=True)
+        )
 
-        self.net = nn.Sequential(*layers)
+        self.normal_res_head = nn.Sequential(
+            nn.ConvTranspose2d(self.init_size * features // pow(2, layer-1), 3, kernel_size=4, stride=2, padding=1)
+        )
 
-        print(self.net)
+        self.high_res_block = nn.Sequential(
+            nn.ConvTranspose2d(self.init_size * features // pow(2, layer-1), self.init_size * features // pow(2, layer), kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(self.init_size * features // pow(2, layer)),
+            nn.ReLU(inplace=True)
+        )
 
-    def forward(self, z):
-        img = self.net(z)
-        return img
+        self.high_res_head = nn.Sequential(
+            nn.ConvTranspose2d(self.init_size * features // pow(2, layer), 3, kernel_size=4, stride=2, padding=1)
+        )
 
+        print(self.network)
+
+    def forward(self, input, alpha):
+        out = self.network(input)
+        image_normal = self.normal_res_head(out)
+        out_high = self.high_res_block(out)
+        image_high = self.high_res_head(out_high)
+
+        image_normal_high = nn.functional.interpolate(image_normal, size=image_high.size()[-2:],mode="bilinear", align_corners=False)
+        image = (1-alpha) * image_normal_high + alpha * image_high
+
+        return image
+
+    def add_layer(self, layer):
+        self.layer = layer
+        self.network.extend(self.high_res_block)
+        self.normal_res_head = self.high_res_head
+        self.high_res_block = nn.Sequential(
+            nn.ConvTranspose2d(self.init_size * features // pow(2, self.layer-1), self.init_size * features // pow(2, self.layer), kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(self.init_size * features // pow(2, self.layer)),
+            nn.ReLU(inplace=True)
+        )
+        self.high_res_head = nn.Sequential(
+            nn.ConvTranspose2d(self.init_size * features // pow(2, self.layer), 3, kernel_size=4, stride=2, padding=1)
+        )
+        print(self.network)
 
 class Discriminator(nn.Module):
-    def __init__(self, img_size, feature_d=64, channels=3):
+    def __init__(self, init_size, img_size, features, channels, layer):
         super(Discriminator, self).__init__()
+        self.init_size = init_size
+        self.features = features
+        self.channels = channels
+        self.layer = layer
 
-        def discriminator_block(in_filters, out_filters, normalize=True):
-            layers = [nn.Conv2d(in_filters, out_filters, 4, 2, 1, bias=False)]
-            if normalize:
-                layers.append(nn.BatchNorm2d(out_filters))
-            layers.append(nn.LeakyReLU(0.2, inplace=True))
-            return layers
+        self.downscales = log2(img_size/self.init_size)
 
-        layers = []
-
-        in_ch = channels
-        out_ch = feature_d
-
-        num_upsamples = int(np.log2(img_size // 8)) + 1
-
-        for i in range(num_upsamples):
-            layers += discriminator_block(in_ch, out_ch, normalize=(i != 0))
-            in_ch = out_ch
-            out_ch = min(out_ch * 2, feature_d * 16)
-
-        layers.append(nn.Conv2d(in_ch, 1, 4, 1, 0))
-        layers.append(nn.Sigmoid())
-
-        self.net = nn.Sequential(*layers)
-        print(self.net)
-
-    def forward(self, img):
-        return self.net(img)
+        print(self.downscales)
 
 
-# Loss function
-criterion = torch.nn.BCELoss()
+        self.network = nn.Sequential(
+            nn.utils.spectral_norm(nn.Conv2d(self.init_size * features // pow(2, self.layer-1), 1, kernel_size=4, stride=1, padding=0)),
+            nn.LeakyReLU(0.2, True),
+        )
 
-# Initialize generator and discriminator
-generator = Generator(opt["img_size"], opt["latent_dim"], 64, channels=opt["channels"])
-discriminator = Discriminator(opt["img_size"], 64, channels=opt["channels"])
+        self.head_normal = nn.Sequential(
+            nn.utils.spectral_norm(nn.Conv2d(3, self.init_size * features // pow(2, self.layer-1), kernel_size=4, stride=2, padding=1)),
+            nn.LeakyReLU(0.2, True),
+        )
 
-if cuda:
-    generator.cuda()
-    discriminator.cuda()
-    criterion.cuda()
+        self.head_high = nn.Sequential(
+            nn.utils.spectral_norm(nn.Conv2d(3, self.init_size * features // pow(2, self.layer), kernel_size=4, stride=2, padding=1)),
+            nn.LeakyReLU(0.2, True)
+        )
+        self.body_high = nn.Sequential(
+            nn.utils.spectral_norm(nn.Conv2d(self.init_size * features // pow(2, self.layer), self.init_size * features // pow(2, self.layer-1), kernel_size=4, stride=2, padding=1)),
+            nn.LeakyReLU(0.2, True),
+        )
 
-# Initialize weights
-generator.apply(weights_init_normal)
-discriminator.apply(weights_init_normal)
+        print(self.network)
+    
+    def forward(self, input_normal, input_high, alpha):
+        pass_high = self.body_high(self.head_high(input_high)) # Downscale once
+        pass_normal = self.head_normal(input_normal)
+        input = (1 - alpha) * pass_normal + alpha * pass_high
 
-# Optimizers
-optimizerG = torch.optim.Adam(generator.parameters(), lr=opt["lr"], betas=(opt["b1"], opt["b2"]))
-optimizerD = torch.optim.Adam(discriminator.parameters(), lr=opt["lr"], betas=(opt["b1"], opt["b2"]))
+        return self.network(input)
 
-fake_label = 0
-real_label = 1
+    def add_layer(self, layer):
+        self.layer = layer
+        self.network.insert(0, self.body_high)
+        self.head_normal = self.head_high
 
-fixed_noise = torch.randn(64, opt["latent_dim"], 1, 1, device=device)
+        self.head_high = nn.Sequential(
+            nn.utils.spectral_norm(nn.Conv2d(3, self.init_size * features // pow(2, self.layer), kernel_size=4, stride=2, padding=1)),
+            nn.LeakyReLU(0.2, True)
+        )
+        self.body_high = nn.Sequential(
+            nn.utils.spectral_norm(nn.Conv2d(self.init_size * features // pow(2, self.layer), self.init_size * features // pow(2, self.layer-1), kernel_size=4, stride=2, padding=1)),
+            nn.LeakyReLU(0.2, True),
+        )
+        print(self.network)
 
-# --- Load dataset ---
-dataset = load_dataset("uoft-cs/cifar10")["train"]
 
-save_dir = "data"  # ImageFolder expects at least one subfolder (class name)
-class_name = "cifar_10"
-class_dir = os.path.join(save_dir, class_name)
-os.makedirs(class_dir, exist_ok=True)
+# --- Image Blending ---
 
-# Save images to disk
-for idx, example in enumerate(dataset):
-    break
-    img = example["img"]
-    if not isinstance(img, Image.Image):
-        img = Image.fromarray(img)
-    img.save(os.path.join(class_dir, f"{idx}.png"))
-print("img saved")
+def seperate_image(image, layer, alpha):
+    image_normal = nn.functional.interpolate(image, size=(pow(2, layer+2), pow(2,layer+2)), mode="bilinear")
+    image_normal_high = nn.functional.interpolate(image_normal, size=(pow(2, layer+3), pow(2,layer+3)), mode="bilinear")
+    image_high = nn.functional.interpolate(image, size=(pow(2, layer+3), pow(2,layer+3)), mode="bilinear")
 
-# Set up transforms
-if opt["channels"] == 1:
-  transform = transforms.Compose([
-      transforms.Resize((opt["img_size"],opt["img_size"])),
-      transforms.Grayscale(),
-      transforms.ToTensor(),
-      transforms.Normalize([0.5], [0.5])
-  ])
-else:
-  transform = transforms.Compose([
-      transforms.Resize((opt["img_size"],opt["img_size"])),
-      transforms.ToTensor(),
-      transforms.Normalize([0.5], [0.5])
-  ])
+    image_blend_high = (1-alpha) * image_normal_high + alpha * image_high
+    image_blend_normal = nn.functional.interpolate(image_blend_high, size=(pow(2, layer+2), pow(2,layer+2)), mode="bilinear")
+    return image_blend_normal, image_blend_high
 
-# Create ImageFolder DataLoader
-dataloader = torch.utils.data.DataLoader(
-    datasets.ImageFolder(
-        root=save_dir,
-        transform=transform,
-    ),
-    batch_size=opt["batch_size"],
-    shuffle=True,
-)
+layer = 1
+
+generator = Generator(init_size, latent_dim, img_size, features, channels, layer)
+discriminator = Discriminator(init_size, img_size, features, channels, layer)
+
+generator.to(device)
+discriminator.to(device)
+
+
+optimizerG = optim.Adam(generator.parameters(), lr=0.001, betas=[0.0, 0.9])
+optimizerD = optim.Adam(discriminator.parameters(), lr=0.001, betas=[0.0, 0.9])
+
+
+alpha = 1.0
+counting_alpha = 0.0
 
 
 
-# ----------
-#  Training
-# ----------
-iters = 0
 
-print("Starting Training Loop...")
-print(f"Running on Device: {device}")
-for epoch in range(opt["n_epochs"]):
-    for i, data in enumerate(dataloader, 0):
 
-        ############################
-        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
-        ###########################
+for ep in range(n_epochs):
+    print(f"Epoch {ep}:")
 
-        ## Train with all-real batch
+
+    i = 0
+    for batch in dataloader:
+        counting_alpha += alpha_incease
+        if (counting_alpha >= alpha_end and layer <= 4):
+            counting_alpha = 0.0
+            alpha_incease *= alpha_dropdown
+            layer += 1
+            generator.add_layer(layer)
+            discriminator.add_layer(layer)
+
+            generator.to(device)
+            discriminator.to(device)
+
+            optimizerG = optim.Adam(generator.parameters(), lr=0.001, betas=[0.0, 0.9])
+            optimizerD = optim.Adam(discriminator.parameters(), lr=0.001, betas=[0.0, 0.9])
+        alpha = min(max(0.0,counting_alpha),1.0)
+        i += 1
         discriminator.zero_grad()
-        # Format batch
-        real_cpu = data[0].to(device)
-        b_size = real_cpu.size(0)
-        label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
-        # Forward pass real batch through D
-        output = discriminator(real_cpu).view(-1)
-        # Calculate loss on all-real batch
-        errD_real = criterion(output, label)
-        # Calculate gradients for D in backward pass
-        errD_real.backward()
-        D_x = output.mean().item()
 
-        ## Train with all-fake batch
-        # Generate batch of latent vectors
-        noise = torch.randn(b_size, opt["latent_dim"], 1, 1, device=device)
-        # Generate fake image batch with G
-        fake = generator(noise)
-        label.fill_(fake_label)
-        # Classify all fake batch with D
-        output = discriminator(fake.detach()).view(-1)
-        # Calculate D's loss on the all-fake batch
-        errD_fake = criterion(output, label)
-        # Calculate the gradients for this batch, accumulated (summed) with previous gradients
-        errD_fake.backward()
-        D_G_z1 = output.mean().item()
-        # Compute error of D as sum over the fake and the real batches
-        errD = errD_real + errD_fake
-        # Update D
+        # Train Discriminator on Real Images
+        real = batch.to(device)
+
+        real_normal, real_high = seperate_image(real, layer, alpha)
+
+        output_real = discriminator(real_normal, real_high, alpha)
+        loss_real = mean(nn.functional.relu(1 - output_real))
+        loss_real.backward()
+
+        
+        # Train Discriminator on Fake Images
+        noise = torch.randn(batch_size, 100, 1, 1).to(device)
+        fake = generator(noise, alpha)
+        fake_normal, fake_high = seperate_image(fake, layer, alpha)
+        output_fake = discriminator(fake_normal, fake_high, alpha) 
+        loss_fake = mean(nn.functional.relu(1 + output_fake))
+        loss_fake.backward()
         optimizerD.step()
+ 
 
-        ############################
-        # (2) Update G network: maximize log(D(G(z)))
-        ###########################
+        # Train Generator with Discriminator
         generator.zero_grad()
-        label.fill_(real_label)  # fake labels are real for generator cost
-        # Since we just updated D, perform another forward pass of all-fake batch through D
-        output = discriminator(fake).view(-1)
-        # Calculate G's loss based on this output
-        errG = criterion(output, label)
-        # Calculate gradients for G
-        errG.backward()
-        D_G_z2 = output.mean().item()
-        # Update G
+        noise = torch.randn(batch_size, 100, 1, 1).to(device)
+        output = generator(noise, alpha)
+        output_normal, output_high = seperate_image(output, layer, alpha)
+        output_fake = discriminator(output_normal, output_high, alpha) 
+        loss_generated = -mean(output_fake)
+        loss_generated.backward()
         optimizerG.step()
 
-        # Output training stats
-        if i % 50 == 0:
-            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
-                  % (epoch, opt["n_epochs"], i, len(dataloader),
-                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
-            torch.save(generator.state_dict(), f'generator-{epoch}.pth')
 
-        # Save Losses for plotting later
 
-        # Check how the generator is doing by saving G's output on fixed_noise
-        if (iters % 500 == 0) or ((epoch == opt["n_epochs"]-1) and (i == len(dataloader)-1)):
-            with torch.no_grad():
-                fake = generator(fixed_noise).detach().cpu()
-                save_image(fake, f"image-{epoch}.png", normalize=True)
+        if i % 16 == 0:
+            print(f"Ep: {ep}, i: {i}/{len(dataloader)}, alpha: {alpha}, D(r): {mean(output_real):.3f}, D(f): {mean(output_fake):.3f}, D Loss: {(loss_real + loss_fake)/2:.3f}, G Loss:  {loss_generated:.3f}")
+        if i % sample_interval == 0:
+            save_image(output, f"image-{ep}.png", normalize=True)
 
-        iters += 1
